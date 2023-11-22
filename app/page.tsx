80266fb6173953e9ce5727bb7cb0466f5c146a4f
Utkarshsingh786 <us8896664511@gmail.com>
2023-11-22 23:25:02 +0530

    new

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <p className='text-3xl font-bold text-indigo-500'>Hello bro</p>
+      <Button>hyy</Button>
+      <span>Heya</span>
+    </>
+=======
+export default function Home() {
+  return (
+    <p className='text-3xl font-bold text-indigo-500'>Hello bro</p>
+>>>>>>> origin/main
+  )
+}
+
+
+
